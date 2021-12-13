339cb374c510bcab8adda98d358c6ea1e11f854e
Po-chia Chen <chen.buo.jia@gmail.com>
2021-12-13 17:40:03 +1100

    Fitting residue-specific CSAs
    Long-overdue work on fitting residue-specific CSAs.

--- calculate-relaxations-from-Ct.py
+++ calculate-relaxations-from-Ct.py
@@ -205,48 +205,98 @@ def optfunc_R1R2NOE_inner(datablock, expblock):
     else:
         return np.mean( np.square( datablock - expblock) )
 
-def optfunc_R1R2NOE_DisoS2CSA(params, *args):
-#def optfunc_r1r2noe( params, args=(relax_obj, num_vecs, s2_list, consts_list, taus_list, vecxh, expblock), full_output=true )
-    Diso=params[0] ; S2s=params[1] ; csa=params[2]
-    robj=args[0]
-    nvecs=args[1] ; S2=args[2] ; consts=args[3] ; taus=args[4]
-    vecxh=args[5] ; w=args[6]
+def optfunc_R1R2NOE_new(params, *args):
+    """
+    Single residue CSA fitting. No global parameters used.
+    
+    """
+#def optfunc_r1r2noe( params, args=(relax_obj, num_vecs, s2_list, consts_list, taus_list, vecXH, expblock), full_output=false )
+    RObj=args[0]
+    S2=args[1] ; consts=args[2] ; taus=args[3]
+    vecXH=args[4] ; w=args[5]
     expblock=args[-1]
-    robj.rotdifModel.change_Diso( Diso )
-    S2loc  = [ S2s*k for k in S2 ]
+    if len(expblock.shape) > 1:
+        exp   = expblock[:,0]
+        sigsq = np.square(expblock[:,1])
+    else:
+        exp   = expblock
+        sigsq = np.zeros(3, dtype=np.float32 )
+
+    if len(vecXH.shape) > 1:
+        # An ensemble of vectors for each site.
+        datablock=np.zeros((3,2), dtype=np.float32)
+        Jmat = sd.J_combine_symmtop_exp_decayN(RObj.omega, vecXH, RObj.rotdifModel.D[0], RObj.rotdifModel.D[1], S2, consts, taus)
+        # = = = Calculate relaxation values from the entire sample of vectors before any averagins is to be done
+        tmpR1, tmpR2, tmpNOE = RObj.get_relax_from_J_simd( Jmat, CSAvalue=params[0] )
+        if w is None:
+            R1 = np.mean(tmpR1)  ; R2 = np.mean(tmpR2)   ; NOE = np.mean(tmpNOE)
+            R1sig = np.std(tmpR1); R2sig = np.std(tmpR2) ; NOEsig = np.std(tmpNOE)
+            datablock[:]=[[R1,R1sig],[R2,R2sig],[NOE,NOEsig]]
+        else:
+            datablock[0]=gm.weighted_average_stdev(tmpR1, w)
+            datablock[1]=gm.weighted_average_stdev(tmpR2, w)
+            datablock[2]=gm.weighted_average_stdev(tmpNOE, w)
+
+        chisq = np.square( datablock[:,0]- exp )
+        sigsq = sigsq + np.square(datablock[:,1])
+        return np.mean( chisq/sigsq )
+    else:
+        #Single XH vector for each site.
+        datablock=np.zeros(3, dtype=np.float32)
+        Jmat = sd.J_combine_symmtop_exp_decayN(RObj.omega, vecXH, RObj.rotdifModel.D[0], RObj.rotdifModel.D[1], S2, consts, taus)
+        R1, R2, NOE = RObj.get_relax_from_J( Jmat, CSAvalue=params[0] )
+        datablock[:]=[R1,R2,NOE]
+        chisq = np.square([R1,R2,NOE] - exp)
+        if sigsq[0] != 0.0 :
+            return  np.mean( chisq/sigsq )
+        else:
+            return np.mean( chisq )
+
+def optfunc_R1R2NOE_DisoS2CSA(params, *args):
+    """
+    3-parameter fitting: Diso, global S2, and global CSA.
+    """
+#def optfunc_r1r2noe( params, args=(relax_obj, num_vecs, s2_list, consts_list, taus_list, vecXH, expblock), full_output=true )
+    Diso=params[0] ; S2s=params[1] ; csa=params[2]
+    RObj=args[0]
+    nVecs=args[1] ; listS2=args[2] ; consts=args[3] ; taus=args[4]
+    vecXH=args[5] ; w=args[6]
+    expblock=args[-1]
+    RObj.rotdifModel.change_Diso( Diso )
+    S2loc  = [ S2s*k for k in listS2 ]
     consts_loc = [ [ S2s*k for k in j] for j in consts ]
-    robj.gX.csa=csa
-    datablock = _obtain_R1R2NOErho( robj, nvecs, S2, consts, taus, vecxh , weights=w)
+    RObj.gX.csa=csa
+    datablock = _obtain_R1R2NOErho( RObj, nVecs, S2loc, consts_loc, taus, vecXH , weights=w)
     chisq = optfunc_R1R2NOE_inner(datablock[0:3,...], expblock)
     print( "= = optimisations params( %s ) returns chi^2 %g" % (params, chisq) )
     return chisq
 
 def optfunc_R1R2NOE_DisoCSA(params, *args):
-#def optfunc_r1r2noe( params, args=(relax_obj, num_vecs, s2_list, consts_list, taus_list, vecxh, expblock), full_output=true )
+#def optfunc_r1r2noe( params, args=(relax_obj, num_vecs, s2_list, consts_list, taus_list, vecXH, expblock), full_output=true )
     Diso=params[0] ; csa = params[1]
-    robj=args[0]
-    nvecs=args[1] ; S2=args[2] ; consts=args[3] ; taus=args[4]
-    vecxh=args[5] ; w=args[6]
+    RObj=args[0]
+    nVecs=args[1] ; S2=args[2] ; consts=args[3] ; taus=args[4]
+    vecXH=args[5] ; w=args[6]
     expblock=args[-1]
-    robj.rotdifModel.change_Diso( Diso )
-    robj.gX.csa=csa
-    datablock = _obtain_R1R2NOErho( robj, nvecs, S2, consts, taus, vecxh, weights=w)
+    RObj.rotdifModel.change_Diso( Diso )
+    RObj.gX.csa=csa
+    datablock = _obtain_R1R2NOErho( RObj, nVecs, S2, consts, taus, vecXH, weights=w)
     chisq = optfunc_R1R2NOE_inner(datablock[0:3,...], expblock)
     print( "= = optimisations params( %s ) returns chi^2 %g" % (params, chisq) )
     return chisq
 
 def optfunc_R1R2NOE_DisoS2(params, *args):
-#def optfunc_r1r2noe( params, args=(relax_obj, num_vecs, s2_list, consts_list, taus_list, vecxh, expblock), full_output=true )
+#def optfunc_r1r2noe( params, args=(relax_obj, num_vecs, s2_list, consts_list, taus_list, vecXH, expblock), full_output=true )
     Diso=params[0] ; S2s=params[1]
-    robj=args[0]
-    nvecs=args[1] ; S2=args[2] ; consts=args[3] ; taus=args[4]
-    vecxh=args[5] ; w=args[6] ; CSAarray=args[7]
+    RObj=args[0]
+    nVecs=args[1] ; S2=args[2] ; consts=args[3] ; taus=args[4]
+    vecXH=args[5] ; w=args[6] ; CSAarray=args[7]
     expblock=args[-1]
-    robj.rotdifModel.change_Diso( Diso )
+    RObj.rotdifModel.change_Diso( Diso )
     S2loc  = [ S2s*k for k in S2 ]
     consts_loc = [ [ S2s*k for k in j] for j in consts ]
     #print( "...Scaling:", consts[0][0], consts_loc[0][0] )
-    datablock = _obtain_R1R2NOErho( robj, nvecs, S2loc, consts_loc, taus, vecxh, weights=w, CSAvaluesArray = CSAarray )
+    datablock = _obtain_R1R2NOErho( RObj, nVecs, S2loc, consts_loc, taus, vecXH, weights=w, CSAvaluesArray = CSAarray )
     chisq = optfunc_R1R2NOE_inner(datablock[0:3,...], expblock)
     print( "= = optimisations params( %s ) returns chi^2 %g" % (params, chisq) )
     return chisq
@@ -255,11 +305,11 @@ def optfunc_R1R2NOE_Diso(params, *args):
 #def optfunc_R1R2NOE( params, args=(relax_obj, num_vecs, S2_list, consts_list, taus_list, vecXH, expblock), full_output=True )
     Diso=params[0]
     RObj=args[0]
-    nvecs=args[1] ; S2=args[2] ; consts=args[3] ; taus=args[4]
+    nVecs=args[1] ; S2=args[2] ; consts=args[3] ; taus=args[4]
     vecXH=args[5] ; w=args[6] ; CSAarray=args[7]
     expblock=args[-1]
     RObj.rotdifModel.change_Diso( Diso )
-    datablock = _obtain_R1R2NOErho( RObj, nvecs, S2, consts, taus, vecXH, weights=w, CSAvaluesArray = CSAarray )
+    datablock = _obtain_R1R2NOErho( RObj, nVecs, S2, consts, taus, vecXH, weights=w, CSAvaluesArray = CSAarray )
     chisq = optfunc_R1R2NOE_inner(datablock[0:3,...], expblock)
     print( "= = Optimisations params( %s ) returns Chi^2 %g" % (params, chisq) )
     return chisq
@@ -496,10 +546,18 @@ if __name__ == '__main__':
                              'Residue-specific variations is set if a file name is given. This file should '
                              'specify on each line the residue index and then its respective CSA value.')
     parser.add_argument('--opt', '--fit', type=str, default=None,
-                        help='Optimise over one or two parameters that perturb the systematic baseline of R1/R2/NOE:'
-                             'the chemical-shift anisotropy of the heavy nucleus (15N), and the global tumbling.'
-                             'This will treat the input D_iso as an initial estimate.'
-                             'Valid inputs: Diso, DisoS2, DisoCSA, none.')
+                        help='Optimise over list of possible parameters that perturb the systematic baseline of R1/R2/NOE:\n'
+                             '  (1) Diso - global tumbling rate,\n'
+                             '  (2) S2   - The zeta-prefactor representing contribution of zero-point vibrations,\n'
+                             '  (3) CSA  - The mean chemical-shift anisotropy of the heavy nucleus.\n'
+                             'Using each will treat the input D_iso as an initial estimate.\n'
+                             'Valid inputs: Diso, DisoS2, DisoCSA, DisoS2CSA, none.')
+    parser.add_argument('--cycles', type=int, default=100,
+                        help='For the new per-residue CSA fitting algorithm, do a maximum of N cycles between the global Diso versus the local CSA fits. '
+                             'Each cycle begins by optimising Diso over all residues, then optimising each CSA separately against the new global value. '
+                             'An additional convergence graph will be created.')
+    parser.add_argument('--tol', type=float, default=1e-5,
+                        help='The tolerance criteria for terminating the global/local optimisation cycles early, as a fractional change.')
     parser.add_argument('--rigid', dest='bRigid', action='store_true',
                         help='Compute the spin-relaxation corresponding to a rigid sphere with rotational diffusion coefficient D_iso/tau_iso, then exit.')
 
@@ -533,6 +591,11 @@ if __name__ == '__main__':
         print( "= = = ERROR: Must give either the background magnetic field or the frequency! E.g., --B0 14.0956", file=sys.stderr )
         sys.exit(1)
 
+    # = = = Paramters for global/local meta-optimisation.
+    nRefinementCycles   = args.cycles
+    refinementTolerance = args.tol
+
+    # = = = Set up the relaxation information from library.
     relax_obj = sd.relaxationModel(nuclei_pair, B0)
     relax_obj.set_time_unit(timeUnit)
     relax_obj.set_freq_relaxation()
@@ -691,6 +754,9 @@ if __name__ == '__main__':
 # = = = Read fitted C(t). For each residue, we expect a set of parameters
 #       corresponding to S2 and the other parameters.
     sim_resid, param_names, param_vals = read_fittedCt_file(fittedCt_file)
+    if len(sim_resid) == 0:
+        print( "= = = ERROR: The fitted-Ct file %s was read, but did not yield any usable parameters!" % fittedCt_file )
+        sys.exit(1)
     num_vecs = len(sim_resid)
     if diff_type == 'symmtop' or diff_type == 'anisotropic':
         sanity_check_two_list(sim_resid, resNH, "resid from fitted_Ct -versus- vectors as defined in anisotropy")
@@ -717,9 +783,9 @@ if __name__ == '__main__':
 
         if bFileFound:
             # = = = Fist check that we're not optimising with CSA.
-            if bOptPars and 'CSA' in optMode:
-                print( "= = = ERROR: Not currently allowing CSA optimisations when a customised CSA file is given.", file=sys.stderr )
-                sys.exit(1)
+            #if bOptPars and 'CSA' in optMode:
+            #    print( "= = = Note: Will turn on per-residue CSA optimisation flag, since a per-resuidue CSA input was given!" )
+            #    bCSAPerResidue = True
 
             residCSA, CSAvaluesArray = gs.load_xy( args.csa )
             relax_obj.gX.csa = np.nan
@@ -769,6 +835,7 @@ if __name__ == '__main__':
     else:
         # = = = Sanity Check
         # Compare the two resid_lists.
+        sim_ind=None
         if not gm.list_identical( sim_resid, exp_resid ):
             print( "= = WARNING: The resids between the simulation and experiment are not the same!", file=sys.stderr )
             print( "...removing elements from the vector files that do not match.", file=sys.stderr )
@@ -789,13 +856,14 @@ if __name__ == '__main__':
             fconsts_list = [ consts_list[x] for x in sim_ind ]
             ftaus_list   = [ taus_list[x] for x in sim_ind ]
             fvecXH       = vecXH.take(sim_ind, axis=0)
+            fCSAs        = CSAvaluesArray.take(sim_ind)
             if not vecXHweights is None:
                 fvecXHweights = vecXHweights.take(sim_ind, axis=0)
             else:
                 fvecXHweights = None
             exp_ind = np.array( [ (np.where(exp_resid==x))[0][0] for x in shared_resid ] )
             expblock = np.take(expblock, exp_ind, axis=1)
-
+            
             print( "Debug (after):", len(fS2_list), fvecXH.shape, expblock.shape )
             print( "(resid)", sim_resid )
         else:
@@ -806,6 +874,7 @@ if __name__ == '__main__':
             ftaus_list   = taus_list
             fvecXH       = vecXH
             fvecXHweights = vecXHweights
+            fCSAs        = CSAvaluesArray
 
         # = = = Do global scan of Diso = = =
         bDoGlobalScan = False
@@ -815,10 +884,71 @@ if __name__ == '__main__':
         else:
             Diso_init = Diso
 
-        if optMode == 'DisoS2CSA':
+        if  optMode == 'new':
+            print( "= = Conducting the global-local refinement... this may take a while." )
+
+            DisoOpt           = Diso_init 
+            fCSAsOpt   = np.copy(fCSAs)
+            fCSAsChiSq = np.zeros( fnum_vecs, dtype=np.float32 )
+            DisoPrev = None ; fCSAsPrev = None
+            bFirst = True
+            #DisoValuesAll = np.zeros( nRefinementCycles, dtype=np.float32 )
+            #DisoChiSqAll  = np.zeros( nRefinementCycles, dtype=np.float32 )
+            #CSAvaluesAll  = np.zeros( (fnum_vecs, nRefinementCycles), dtype=np.float32 )
+            #CSAChiSqAll   = np.zeros( (fnum_vecs, nRefinementCycles), dtype=np.float32 )
+            for r in range(nRefinementCycles):
+                # = = = Global Stage
+                out = fmin_powell(optfunc_R1R2NOE_Diso, x0=DisoOpt, direc=[0.1*DisoOpt], \
+                    args=(relax_obj, fnum_vecs, fS2_list, fconsts_list, ftaus_list, fvecXH, fvecXHweights, CSAvaluesArray, expblock), full_output=True )
+                DisoOpt = out[0] ; ChiSqDiso = out[1]
+                #DisoValuesAll[r] = DisoOpt
+                #DisoChiSqAll[r]  = ChiSqDiso
+                if (not bFirst) and np.allclose(DisoOpt, DisoPrev, rtol=refinementTolerance):
+                    print("= = = BREAK at Diso test.")
+                    break
+                DisoPrev = DisoOpt
+
+                # = = = Local Stage
+                for i in range(fnum_vecs):
+                    out = fmin_powell( optfunc_R1R2NOE_new, x0=fCSAsOpt[i], \
+                        args=(relax_obj, fS2_list[i], fconsts_list[i], ftaus_list[i], fvecXH[i], fvecXHweights[i], expblock[:,i,:]), full_output=True )
+                    fCSAsOpt[i] = out[0] ; fCSAsChiSq[i] = out[1]
+                #CSAvaluesAll[:,r] = CSAvaluesArrayOpt
+                #CSAChiSqAll[:,r]  = CSAChiSqOpt
+
+                if (not bFirst) and np.allclose(fCSAsOpt, fCSAsPrev, rtol=refinementTolerance):
+                    print("= = = BREAK at CSA test")
+                    break
+                fCSAsPrev = fCSAsOpt
+                # = = = Final check
+                if bFirst:
+                    bFirst=False
+                print("    ...round %i complete." % r)
+
+            print("    ....optimisation complete at round %i." % r)
+            #gs.print_xy('temp_Diso_value.xvg', np.arange(1,r+1), DisoValuesAll[:r])
+            #gs.print_xy('temp_Diso_chi.xvg', np.arange(1,r+1), DisoChiSqAll[:r])
+            #gs.print_xylist('temp_CSA_value.xvg', np.arange(1,r+1), CSAvaluesAll[:,:r])
+            #gs.print_xylist('temp_CSA_chi.xvg', np.arange(1,r+1), CSAvaluesAll[:,:r])
+            optHeader=print_fitting_params_headers(names=param_names,
+                      values=np.multiply(param_scaling, (DisoOpt, 1.0, np.nan, np.sqrt(ChiSqDiso)) ),
+                      units=param_units,
+                      bFit=(True, False, False, True) )
+            optHeader=optHeader+"\n# See %s_CSA_values.xvg for individual CSA optimisations." % out_pref
+            print( optHeader )
+
+            # = = = Copy back over to overall array
+            if not sim_ind is None:
+                for i,j in enumerate(sim_ind):
+                    CSAvaluesArray[j] = fCSAsOpt[i]
+            else:
+                CSAvaluesArray = fCSAsOpt
+            gs.print_xy(out_pref+'_CSA_values.xvg', sim_resid, CSAvaluesArray )
+
+        elif optMode == 'DisoS2CSA':
             print( "= = Fitting both Diso, S2, as well as average CSA.." )
             p_init=( Diso_init, 1.0, relax_obj.gX.csa )
-            # The directions are set like this because CSA and S2 both compensate for Diso.
+            # The Powell minimisation search directions are set like this because CSA and S2 both compensate for Diso.
             dmat=np.array([[ np.sqrt(1.0/3.0), np.sqrt(1.0/3.0), np.sqrt(1.0/3.0)],
                            [-np.sqrt(2.0/3.0), np.sqrt(1.0/6.0), np.sqrt(1.0/6.0)],
                            [                0, np.sqrt(1.0/2.0),-np.sqrt(1.0/2.0)]])
@@ -840,7 +970,7 @@ if __name__ == '__main__':
             print( "= = Fitting both Diso and the average CSA.." )
             # = = = We need to fit two parameters: D_iso and CSA
             p_init=( Diso_init, relax_obj.gX.csa )
-            # The directions are set like this because CSA and Diso compensate for each other's effects.
+            # The Powell minimisation search directions are set like this because CSA and Diso compensate for each other's effects.
             d_init=( (0.1*p_init[0], 0.1*p_init[1]), (0.1*p_init[0], -0.1*p_init[1]) )
             fminOut=fmin_powell( optfunc_R1R2NOE_DisoCSA, x0=p_init, direc=d_init, \
                 args=(relax_obj, fnum_vecs, fS2_list, fconsts_list, ftaus_list, fvecXH, fvecXHweights, expblock), full_output=True )
@@ -854,7 +984,7 @@ if __name__ == '__main__':
                       bFit=(True, False, True, True) )
             print( optHeader )
         elif optMode == 'DisoS2':
-            print( "= = Fitting both D_iso and overal S2 scaling.." )
+            print( "= = Fitting both D_iso and overall S2 scaling.." )
             p_init=( Diso_init, 1.0 )
             d_init=( (0.1*p_init[0], 0.1*p_init[1]), (0.1*p_init[0], -0.1*p_init[1]) )
             # The directions are set like this because S2 and Diso compensate for each other's effects.
